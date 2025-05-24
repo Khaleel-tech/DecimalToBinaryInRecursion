c910f8d4d06d5c12abf6b38c5284a11024346814
khaleel <shaikkhaleel1425@gmail.com>
2025-05-24 11:28:05 +0530

    Decimal to Binary implementation in C, C++, Python

---
+++ DecToBin.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int DecToBin(int num){
+    int ans = 0 ;//this will store our final answer
+    int power = 1 ;//this is used to multiply in order to get answer, 10^0,10^1,10^2
+
+    // now loop
+    while (num>0){
+        int rem = num % 2;
+        num /= 2;
+        ans += (rem * power);
+        power *= 10;
+    }
+    return ans;
+}
+
+int main(){
+    int ExNum = 24;
+    printf("%d\n",DecToBin(ExNum));
+    return 0;
+}
